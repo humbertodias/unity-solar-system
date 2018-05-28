5b1505350ad0072f2a07305189a4601b8ba20efb
Unknown <humberto.dias@techne.com.br>
2018-05-27 22:18:18 -0300

    Unity 2018.1.1f1

---
+++ Assets/SolarSystem/Scripts/Rotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour
+{
+
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+    // Rotate the object around its local Y axis at 1 degree per second
+    transform.Rotate(Vector3.right * Time.deltaTime);
+
+  }
+
+}
